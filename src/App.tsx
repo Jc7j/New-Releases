6ec62b2f7cead6fb766416c6012f8606b32848a5
Jc7j <chiangjason19@gmail.com>
2020-08-31 15:33:49 -0700

    Added ability to login into spotify and redirect to a different page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 
-import './App.css';
+import Login from './pages/Login';
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+import { getTokenFromResponse } from './utils';
+
+const s = new SpotifyWebApi();
+
+const App = () => {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = '';
+    //@ts-ignore
+    let _token = hash.access_token;
+
+    if (_token) {
+      s.setAccessToken(_token);
+      setToken(_token);
+    }
+  }, [token]);
+
+  return (
+    <div className="App">
+      {!token && <Login />}
+      {token && <h1>Hello world</h1>}
+    </div>
+  );
+};
 
 export default App;
