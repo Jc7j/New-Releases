4ccf86181065fd847a864f11cb0c171d3bc7422d
Jc7j <chiangjason19@gmail.com>
2020-09-04 21:32:14 -0700

    Successfully displayed album name

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,31 @@
-import React, { useEffect } from 'react';
-import SpotifyWebApi from 'spotify-web-api-js';
+import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 
-import '../styles/HomePage.css';
+import "../styles/HomePage.css";
 
 interface HomePageProps {}
 
 const s = new SpotifyWebApi();
 
 const HomePage: React.FC<HomePageProps> = () => {
+  let [albumsList, setAlbumsList] = useState<any>([]);
+
   useEffect(() => {
-    s.setAccessToken(localStorage.getItem('spotifyToken'));
-    // s.getPlaylist('37i9dQZEVXcJZyENOWUFo7').then((response: any) =>
-    //   console.log(response)
-    // );
-    // s.getMe().then((response: any) => {
-    //   console.log(response);
-    // });
-  });
-  return <div className="HomePageContainer">hello</div>;
+    s.setAccessToken(localStorage.getItem("spotifyToken"));
+
+    s.getNewReleases().then((res: any) => {
+      setAlbumsList(res["albums"]["items"]);
+    });
+  }, []);
+
+  return (
+    <div className="HomePageContainer">
+      {albumsList &&
+        albumsList.map((obj: any) => {
+          return <p>{obj["name"]}</p>;
+        })}
+    </div>
+  );
 };
 
 export default HomePage;
