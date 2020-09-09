3c0116cddf24643fd7883b5dc8d9edf3a7c7e3e9
Jc7j <chiangjason19@gmail.com>
2020-09-08 21:40:08 -0700

    Created Tabs, Tab, Panel components

---
+++ src/components/Tab.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useTabs } from "./Tabs";
+
+import "../styles/Tab.css";
+
+export interface ITabProps {
+  label: string;
+}
+
+/**
+ * This component allows changing of the active Tab.
+ */
+export const Tab: React.FC<ITabProps> = (props) => {
+  const { setActiveTab } = useTabs();
+  return (
+    <div>
+      <button className="TabButton" onClick={() => setActiveTab(props.label)}>
+        {props.children}
+      </button>
+    </div>
+  );
+};
