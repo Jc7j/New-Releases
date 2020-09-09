9eb2b15d791d2864c1d8c26a19a02cd673ade4e2
Jc7j <chiangjason19@gmail.com>
2020-09-08 20:43:13 -0700

    Set Profile picture, User Name and HomePage positioning

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,66 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import SpotifyWebApi from 'spotify-web-api-js';
+import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 
-import SideNavbar from '../components/SideNavbar';
+import SideNavbar from "../components/SideNavbar";
 
-import '../styles/HomePage.css';
+import "../styles/HomePage.css";
 
 interface HomePageProps {}
 
 const s = new SpotifyWebApi();
-s.setAccessToken(localStorage.getItem('spotifyToken'));
+s.setAccessToken(localStorage.getItem("spotifyToken"));
 
 const HomePage: React.FC<HomePageProps> = () => {
   const [albumsList, setAlbumsList] = useState<any>([]);
-  const [userName, setUserName] = useState<string>('');
+  const [userName, setUserName] = useState<string>("");
+  const [profileImage, setProfileImg] = useState<string>("");
+
   useEffect(() => {
     s.getNewReleases().then((res: any) => {
-      if (res) setAlbumsList(res['albums']['items']);
+      if (res) setAlbumsList(res["albums"]["items"]);
     });
+
     s.getMe().then((res: any) => {
       console.log(res);
-      // if (res) setUserName(res['display_name']);
+      if (res) {
+        setUserName(res["display_name"]);
+        setProfileImg(res["images"][0]["url"]);
+      }
     });
   }, []);
 
-  const onClickHandle = () => {
-    window.location.reload(true);
-  };
+  // const onClickHandler = () => {
+  //   window.location.reload(true);
+  // };
 
   return (
     <div className="HomePageContainer">
       <SideNavbar />
+      <div className="SecondContainer">
+        <div className="ProfileContainer">
+          <img src={profileImage} alt="Profile Pic" />
+          <div>
+            <h1>{userName}</h1>
+          </div>
+        </div>
+        <div className="DisplayContainer">hi</div>
+      </div>
     </div>
   );
 };
 
 export default HomePage;
 
-{
-  // <h1>Hi {userName}! Here are some new releases on Spotify:</h1>
-  /*<button className="HomePageButton" onClick={onClickHandle}>
-  Reload if New Releases are not shown
-</button>
-<div className="NewReleasesContainer">
-  {albumsList &&
-    albumsList.map((obj: any) => {
-      return (
-        <div className="IndividualReleaseContainer" key={obj['id']}>
-          <div className="ReleaseTitle">
-            <h3>
-              {obj['name']} by <i>{obj['artists'][0]['name']}</i>
-            </h3>
-            <p>Release Date: {obj['release_date']}</p>
-          </div>
-          <a href={obj['uri']}>
-            <img
-              src={obj['images'][0]['url']}
-              alt="Album Pic"
-              className="ImageContainer"
-            />
-          </a>
-        </div>
-      );
-    })}
-</div> */
-}
+// {
+//    <h1>Hi {userName}! Here are some new releases on Spotify:</h1>
+//   <button className="HomePageButton" onClick={onClickHandle}>
+//   Reload if New Releases are not shown
+// </button>
+// <div className="NewReleasesContainer">
+//   {albumsList &&
+//     albumsList.map((obj: any) => {
+//       return (
+//         <div className="IndividualReleaseContainer" key={obj['id']}>
+//           <div className="ReleaseTitle">
+//             <h3>
+//               {obj['name']} by <i>{obj['artists'][0]['name']}</i>
+//             </h3>
+//             <p>Release Date: {obj['release_date']}</p>
+//           </div>
+//           <a href={obj['uri']}>
+//             <img
+//               src={obj['images'][0]['url']}
+//               alt="Album Pic"
+//               className="ImageContainer"
+//             />
+//           </a>
+//         </div>
+//       );
+//     })}
+// </div>
+// }
